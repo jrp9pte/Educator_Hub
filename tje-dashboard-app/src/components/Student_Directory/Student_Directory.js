2e70579e9a165bcb93b30a8c380e02472ad9facf
yujennny <yu34j@mtholyoke.edu>
2023-05-25 05:24:15 +0900

    Class list fixed

--- tje-dashboard-app/src/components/Student_Directory/Student_Directory.js
+++ tje-dashboard-app/src/components/Student_Directory/Student_Directory.js
@@ -1,28 +1,46 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { db } from './../../firebase.js';
-import { collection, getDocs } from 'firebase/firestore';
+import { collection, getDocs, addDoc } from 'firebase/firestore';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
+import { TextField } from '@mui/material';
+import Button from "@mui/material/Button";
+
+
+
+async function addNewStudent(studentName, studentAge) {
+  const docRef = await addDoc(collection(db, "Students"), {
+    name: studentName,
+    age: studentAge
+    // must add the class and the grade also 
+    // Maybe make class a dropdown menu attached to the class id
+  });
+}
+
 
 function GetClass (classes, id) {
   let className = "";
-  for (let i =0; i<classes.length; i++) {
-    if (classes[i].id === id) {
-      className = classes[i].name
-    }
 
-    return className;
-  }
-  
+  classes.filter(cla => cla.id  === id).map(filteredClass => (
+    className = filteredClass.name
+  ));
+
+
+  return className;
+
 }
 function Student_Directory() {
   const studentColRef = collection(db, "Students");
   const classRef = collection(db, "Classes");
   const [studentList, setStudentList] = useState([]);
   const [classList, setClassList] = useState([]);
+  const [studentName, setStudentName] = useState("");
+  const [studentAge, setStudentAge] = useState(0);
+  const [studentAdded, setStudentAdded] = useState(false);
+  const [studentClass, setStudentClass] = useState([]);
 
   useEffect(() => {
     const getClassList = async () => {
@@ -63,7 +81,41 @@ function Student_Directory() {
     <div style={{ textAlign: "center" }}>
       <h1>Student Directory</h1>
       <Link to="/">Home</Link>
+      <header>
+        <br></br>
+      <Button
+          variant="outlined"
+          onClick={() => {
+            addNewStudent(studentName, studentAge);
+            setStudentAdded(!studentAdded);
+          }}
+        >
+          Add New Student: 
+        </Button>
+        <br></br>
+        <br></br>
+      <TextField
+          value={studentName}
+          onChange={(e) => setStudentName(e.target.value)}
+          label="Student Name"
+          variant="outlined"
+        />
+        <TextField
+          value={studentAge}
+          onChange={(e) => setStudentAge(e.target.value)}
+          label="Student age"
+          variant="outlined"
+        />
+        <TextField
+          value={studentClass}
+          onChange={(e) => setStudentClass(e.target.value)}
+          label="Class"
+          variant="outlined"
+        />
+        
+      </header>
       <div>
+        <br></br>
         <Box sx= {{ minWidth : 275}}>
           
         {studentList.map((data) => (
@@ -79,8 +131,7 @@ function Student_Directory() {
               Classes Taken: 
               {data.classesTaken.map( (c) => (
                 <div key = {c.id}>
-                  {/* {GetClass(classList, c.class.id)} */}
-                  
+                  {GetClass(classList, c.class.id)}
                 </div>
               ))}
               </Typography>
