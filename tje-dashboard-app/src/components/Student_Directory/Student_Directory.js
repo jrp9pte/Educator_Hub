99a27705392005514deec94ae200cf493a13180a
yujennny <yu34j@mtholyoke.edu>
2023-05-25 03:17:10 +0900

    Added mui

--- tje-dashboard-app/src/components/Student_Directory/Student_Directory.js
+++ tje-dashboard-app/src/components/Student_Directory/Student_Directory.js
@@ -4,9 +4,7 @@ import { db } from './../../firebase.js';
 import { collection, getDocs } from 'firebase/firestore';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
 function GetClass (classes, id) {
