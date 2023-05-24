9c0cc57b82241fe2289bf7aaea25f22604c6d8ce
yujennny <yu34j@mtholyoke.edu>
2023-05-24 23:08:09 +0900

    Added table display

--- tje-dashboard-app/src/components/Student_Directory/Student_Directory.js
+++ tje-dashboard-app/src/components/Student_Directory/Student_Directory.js
@@ -1,12 +1,36 @@
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { db } from './../../firebase.js';
+import { collection, getDocs } from 'firebase/firestore';
+import Table from "./Table";
 
-function STUDENT_DIRECTORY() {
+function Student_Directory() {
+  const studentColRef = collection(db, "Students");
+  const [studentList, setStudentList] = useState([]);
+  useEffect(() => {
+    const getStudentList = async () => {
+      try {
+        const data = await getDocs(studentColRef);
+        const filteredData = data.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+        setStudentList(filteredData);
+        console.log(filteredData);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getStudentList();
+  }, [studentColRef]);
+
   return (
     <div style={{ textAlign: "center" }}>
       <h1>Student Directory</h1>
       <Link to="/">Home</Link>
+      {Table(studentList)}
     </div>
   );
 }
 
-export default STUDENT_DIRECTORY;
+export default Student_Directory;
