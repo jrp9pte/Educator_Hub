672a224be82cf9b401b821cf381fe647de8724cb
Jacob Wald <vug5ue@virginia.edu>
2023-05-25 11:29:40 -0400

    Added button to home page

--- tje-dashboard-app/src/components/HOME/HOME.js
+++ tje-dashboard-app/src/components/HOME/HOME.js
@@ -3,18 +3,32 @@ import { Link } from "react-router-dom";
 import React from "react";
 import { Container, Row, Col, Image } from "react-bootstrap";
 import LoginForm from "../AUTH/LoginForm";
+import Button from "@mui/material/Button";
 
 function HOME() {
   return (
     <div style={{ textAlign: "center" }}>
       <h1>Home Page</h1>
-      <Link to="/overall_dashboard">Dashboard</Link>
+      <Link to="/overall_dashboard">
+        <Button variant="contained">Dashboard</Button>
+      </Link>
       <span style={{ margin: "0 10px" }}>|</span>
-      <Link to="/student_directory">Student Directory</Link>
+      <Link to="/student_directory">
+        {" "}
+        <Button variant="contained">Student Directory</Button>
+      </Link>
       <span style={{ margin: "0 10px" }}>|</span>
-      <Link to="/teacher_directory">Teacher Directory</Link>
+      <Link to="/teacher_directory">
+        {" "}
+        <Button variant="contained">Teacher Directory</Button>
+      </Link>
       <span style={{ margin: "0 10px" }}>|</span>
-      <Link to="/calendar">Calendar</Link>
+      <Link to="/calendar">
+        {" "}
+        <Button variant="contained">Calendar</Button>
+      </Link>
+      <br></br>
+      <br></br>
       <LoginForm />
     </div>
   );
