addce49e6e52676d71e70bb70dd7a00d0d46b8ba
Tanisha Mehta <tanishamehta@Tanishas-MacBook-Pro.local>
2023-05-25 22:42:11 +0800

    minor changes

--- tje-dashboard-app/src/components/HOME/HOME.js
+++ tje-dashboard-app/src/components/HOME/HOME.js
@@ -13,20 +13,9 @@ function HOME() {
           <Link to="/student_directory">Student Directory</Link>
           <span style={{ margin: "0 10px" }}>|</span>
           <Link to="/teacher_directory">Teacher Directory</Link>
-          <LoginForm />
-        <Row>
-        <Col>
-          <iframe
-            style={{margin:'5%'}}
-            src="https://calendar.google.com/calendar/embed?src=c_d45a5e6cf0d4fedfe804fd8f39269699f596aa178cd2c3aa53cb1efec73944fb%40group.calendar.google.com&ctz=America%2FNew_York"
-            width="700"
-            height="500"
-            scrolling="no"
-          ></iframe>
-        </Col>
-        </Row>
           <span style={{ margin: "0 10px" }}>|</span>
           <Link to="/calendar">Calendar</Link>
+          <LoginForm />
         </div>
   );
 }
