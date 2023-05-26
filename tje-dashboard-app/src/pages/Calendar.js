401951320ffc769fa3cbf1459689349d92f4221f
Jacob Wald <vug5ue@virginia.edu>
2023-05-25 20:23:07 -0400

    Calendar button fixed

--- tje-dashboard-app/src/pages/Calendar.js
+++ tje-dashboard-app/src/pages/Calendar.js
@@ -1,24 +1,27 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
 
-import {Row, Col} from "react-bootstrap"
+import { Row, Col } from "react-bootstrap";
+import Button from "@mui/material/Button";
 function Calendar() {
-    return(
+  return (
     <div style={{ textAlign: "center" }}>
-        <h1>Calendar</h1>
-        <Link to="/Home">Home</Link>
-        <Row>
+      <h1>Calendar</h1>
+      <Link to="/Home">
+        <Button variant="contained">Home</Button>
+      </Link>
+      <Row>
         <Col>
           <iframe
-            style={{margin:'5%'}}
+            style={{ margin: "5%" }}
             src="https://calendar.google.com/calendar/embed?src=c_d45a5e6cf0d4fedfe804fd8f39269699f596aa178cd2c3aa53cb1efec73944fb%40group.calendar.google.com&ctz=America%2FNew_York"
             width="700"
             height="500"
             scrolling="no"
           ></iframe>
         </Col>
-        </Row>
-        </div>
-    );
+      </Row>
+    </div>
+  );
 }
 export default Calendar;
