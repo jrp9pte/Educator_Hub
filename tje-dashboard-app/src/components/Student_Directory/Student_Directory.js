e689dd26fcf3cf40fe28005d25a51bcad35a3dc1
Jacob Wald <vug5ue@virginia.edu>
2023-05-25 20:19:41 -0400

    Student Directory error fixed. Styling added to Header.

--- tje-dashboard-app/src/components/Student_Directory/Student_Directory.js
+++ tje-dashboard-app/src/components/Student_Directory/Student_Directory.js
@@ -1,19 +1,26 @@
-
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { db } from './../../firebase.js';
-import { collection, getDocs, addDoc, query, where, doc, deleteDoc, updateDoc } from 'firebase/firestore';
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
-import { TextField } from '@mui/material';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { db } from "./../../firebase.js";
+import {
+  collection,
+  getDocs,
+  addDoc,
+  query,
+  where,
+  doc,
+  deleteDoc,
+  updateDoc,
+} from "firebase/firestore";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import { TextField } from "@mui/material";
 import Button from "@mui/material/Button";
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
-
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 
 async function addNewStudent(studentName, studentAge, sClass, allClass) {
   let classInfo = [];
@@ -35,35 +42,28 @@ async function addNewStudent(studentName, studentAge, sClass, allClass) {
   });
 }
 
-
-async function removeStudent (allStudent, studentToRemove) {
+async function removeStudent(allStudent, studentToRemove) {
   let delStudent = allStudent.filter(
     (student) => student.name === studentToRemove
   );
 
   const docRef = await deleteDoc(doc(db, "Students", delStudent[0].id));
-
 }
 
-async function editStudent (allStudent, editName, editAge, studentChange) {
+async function editStudent(allStudent, editName, editAge, studentChange) {
   try {
-    let editS = allStudent.filter(
-    (student) => student.name === studentChange
-  );
+    let editS = allStudent.filter((student) => student.name === studentChange);
 
-
-  const docRef = await updateDoc(doc(db, "Students", editS[0].id), {
-    name: editName,
-    age: editAge
-  });
+    const docRef = await updateDoc(doc(db, "Students", editS[0].id), {
+      name: editName,
+      age: editAge,
+    });
   } catch (err) {
     console.error(err);
   }
-  
 }
 
-function GetClass (classes, id) {
-
+function GetClass(classes, id) {
   let className = "";
 
   classes
@@ -76,9 +76,9 @@ function GetClass (classes, id) {
 function GetClassId(classes, className) {
   let classId = "";
 
-  classes.filter(cla => cla.name  === className).map(filteredId => (
-    classId = filteredId.id
-  ));
+  classes
+    .filter((cla) => cla.name === className)
+    .map((filteredId) => (classId = filteredId.id));
 
   return classId;
 }
@@ -124,7 +124,7 @@ function Student_Directory() {
     };
 
     getClassList();
-  }, [classRef, classList]);
+  }, []);
 
   useEffect(() => {
     const getStudentList = async () => {
@@ -141,176 +141,224 @@ function Student_Directory() {
       }
     };
     getStudentList();
-  }, [studentAdded, studentRemoved, studentEdited, studentColRef]);
+  }, [studentAdded, studentRemoved, studentEdited]);
 
   return (
     <div style={{ textAlign: "center" }}>
       <h1>Student Directory</h1>
-      <Link to="/Home">Home</Link>
+      <Link to="/Home">
+        <Button variant="contained">Home</Button>
+      </Link>
       <header>
-        <br></br>
-        <Button
-          variant="outlined"
-          onClick={() => {
-            if (studentName !== "") {
-              addNewStudent(studentName, studentAge, studentClass, classList);
-              setStudentAdded(!studentAdded);
-            }
+        <h2>Edit A Student</h2>
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "1fr 1fr 1fr",
+            justifyContent: "center",
           }}
         >
-          Add New Student
-        </Button>
-        <br></br>
-        <br></br>
-        <TextField
-          value={studentName}
-          onChange={(e) => setStudentName(e.target.value)}
-          label="Student Name"
-          variant="outlined"
-        />
-        <TextField
-          value={studentAge}
-          onChange={(e) => setStudentAge(e.target.value)}
-          label="Student age"
-          variant="outlined"
-        />
-        <Box>
-          <FormControl size="medium">
-            <InputLabel id="demo-simple-select-label">Classes</InputLabel>
-            <Select
-              labelId="demo-multiple-select-label"
-              id="demo-multiple-select"
-              multiple
-              value={studentClass}
-              label="Classes"
-              onChange={handleChange}
-            >
-              {classList.map((oneClass) => (
-                <MenuItem key={oneClass.id} value={oneClass.name}>
-                  {" "}
-                  {oneClass.name}{" "}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-        </Box>
+          <div>
+            <h4>Add New Student</h4>
+            <div>
+              <div style={{ display: "flex", justifyContent: "center" }}>
+                <TextField
+                  value={studentName}
+                  onChange={(e) => setStudentName(e.target.value)}
+                  label="Student Name"
+                  variant="outlined"
+                  style={{ marginRight: "5px" }}
+                />
+                <TextField
+                  value={studentAge}
+                  onChange={(e) => setStudentAge(e.target.value)}
+                  label="Student age"
+                  variant="outlined"
+                />
+              </div>
+              <br></br>
+              <div style={{ display: "flex", justifyContent: "center" }}>
+                <Box
+                  sx={{
+                    width: 225,
+                  }}
+                >
+                  <FormControl fullWidth>
+                    <InputLabel id="demo-simple-select-label">
+                      Classes
+                    </InputLabel>
+                    <Select
+                      labelId="demo-multiple-select-label"
+                      id="demo-multiple-select"
+                      multiple
+                      value={studentClass}
+                      label="Classes"
+                      onChange={handleChange}
+                    >
+                      {classList.map((oneClass) => (
+                        <MenuItem key={oneClass.id} value={oneClass.name}>
+                          {" "}
+                          {oneClass.name}{" "}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  </FormControl>
+                </Box>
+              </div>
+              <br></br>
+              <Button
+                variant="outlined"
+                onClick={() => {
+                  if (studentName !== "") {
+                    addNewStudent(
+                      studentName,
+                      studentAge,
+                      studentClass,
+                      classList
+                    );
+                    setStudentAdded(!studentAdded);
+                  }
+                }}
+              >
+                Add New Student
+              </Button>
+            </div>
+          </div>
+          <div>
+            <h4>Remove A Student</h4>
+            <div>
+              <div style={{ display: "flex", justifyContent: "center" }}>
+                <Box
+                  sx={{
+                    width: 225,
+                  }}
+                >
+                  <FormControl fullWidth>
+                    <InputLabel id="demo-simple-select-label">
+                      Students
+                    </InputLabel>
+                    <Select
+                      labelId="demo-simple-select-label"
+                      id="demo-simple-select"
+                      value={remStudent}
+                      label="student"
+                      onChange={(e) => setRemStudent(e.target.value)}
+                    >
+                      {studentList.map((s) => (
+                        <MenuItem key={s.id} value={s.name}>
+                          {" "}
+                          {s.name}{" "}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  </FormControl>
+                </Box>
+              </div>
+              <br></br>
+              <div style={{ display: "flex", justifyContent: "center" }}>
+                <Button
+                  variant="outlined"
+                  onClick={() => {
+                    if (remStudent !== "") {
+                      removeStudent(studentList, remStudent);
+                      setStudentRemoved(!studentAdded);
+                    }
+                  }}
+                >
+                  Delete Student
+                </Button>
+              </div>
+            </div>
+          </div>
+          <div>
+            <h4>Edit Student Information</h4>
+            <div>
+              <div style={{ display: "flex", justifyContent: "center" }}>
+                <Box
+                  sx={{
+                    width: 225,
+                  }}
+                >
+                  <FormControl fullWidth>
+                    <InputLabel id="demo-simple-select-label">
+                      Students
+                    </InputLabel>
+                    <Select
+                      labelId="demo-simple-select-label"
+                      id="demo-simple-select"
+                      value={studentToEdit}
+                      label="student"
+                      onChange={(e) => setStudentToEdit(e.target.value)}
+                    >
+                      {studentList.map((s) => (
+                        <MenuItem key={s.id} value={s.name}>
+                          {" "}
+                          {s.name}{" "}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  </FormControl>
+                </Box>
+              </div>
+              <br></br>
+              <div style={{ display: "flex", justifyContent: "center" }}>
+                <TextField
+                  value={newName}
+                  onChange={(e) => setNewName(e.target.value)}
+                  label="New Student Name"
+                  variant="outlined"
+                  style={{ marginRight: "5px" }}
+                />
+                <TextField
+                  value={newAge}
+                  onChange={(e) => setNewAge(e.target.value)}
+                  label="New student age"
+                  variant="outlined"
+                />
+              </div>
+              <br></br>
+              <div style={{ display: "flex", justifyContent: "center" }}>
+                <Button
+                  variant="outlined"
+                  onClick={() => {
+                    if (newName !== "") {
+                      editStudent(studentList, newName, newAge, studentToEdit);
+                      setStudentEdited(!studentEdited);
+                    }
+                  }}
+                >
+                  Edit student information
+                </Button>
+              </div>
+            </div>
+          </div>
+        </div>
       </header>
       <br></br>
-      <br></br>
-
-      <Button
-          variant="outlined"
-          onClick={() => {
-            if(remStudent !== "") {
-            removeStudent(studentList, remStudent);
-            setStudentRemoved(!studentAdded);
-            }
-          }}
-        >
-          Delete Student
-        </Button>
-      <Box
-            >
-              <FormControl size = 'medium' >
-                <InputLabel id="demo-simple-select-label">
-                  Students
-                </InputLabel>
-                <Select
-                  labelId="demo-simple-select-label"
-                  id="demo-simple-select"
-                  value={remStudent}
-                  label="student"
-                  onChange={(e) => setRemStudent(e.target.value)}
-                >
-
-                  {studentList.map((s) => (
-                    <MenuItem key={s.id} value={s.name}>
-                      {" "}
-                      {s.name}{" "}
-                    </MenuItem>
-                  ))}
-                </Select>
-              </FormControl>
-            </Box>
-
+      <Box sx={{ minWidth: 275 }}>
+        {studentList.map((data) => (
+          <div key={data.id}>
+            <Card variant="outlined">
+              <React.Fragment>
+                <CardContent>
+                  <Typography sx={{ fontSize: 16 }} gutterBottom>
+                    {data.name}, Age: {data.age}
+                  </Typography>
+                  <br></br>
+                  <Typography sx={{ fontSize: 14 }}>
+                    Classes Taken:
+                    {data.classesTaken.map((c) => (
+                      <div key={c.id}>{GetClass(classList, c.class.id)}</div>
+                    ))}
+                  </Typography>
+                  <br></br>
+                </CardContent>
+              </React.Fragment>
+            </Card>
             <br></br>
-            <br></br>
-            <Button
-          variant="outlined"
-          onClick={() => {
-            if(newName !== "") {
-            editStudent(studentList, newName, newAge, studentToEdit);
-            setStudentEdited(!studentEdited);
-            }
-          }}
-        >
-          Edit student information
-        </Button>
-        <Box
-            >
-              <FormControl size = 'medium' >
-                <InputLabel id="demo-simple-select-label">
-                  Students
-                </InputLabel>
-                <Select
-                  labelId="demo-simple-select-label"
-                  id="demo-simple-select"
-                  value={studentToEdit}
-                  label="student"
-                  onChange={(e) => setStudentToEdit(e.target.value)}
-                >
-
-                  {studentList.map((s) => (
-                    <MenuItem key={s.id} value={s.name}>
-                      {" "}
-                      {s.name}{" "}
-                    </MenuItem>
-                  ))}
-                </Select>
-              </FormControl>
-            </Box>
-            <TextField
-          value={newName}
-          onChange={(e) => setNewName(e.target.value)}
-          label="New Student Name"
-          variant="outlined"
-        />
-        <TextField
-          value={newAge}
-          onChange={(e) => setNewAge(e.target.value)}
-          label="New student age"
-          variant="outlined"
-        />
-        <br></br>
-        <Box sx={{ minWidth: 275 }}>
-          {studentList.map((data) => (
-            <div key={data.id}>
-              <Card variant="outlined">
-                <React.Fragment>
-                  <CardContent>
-                    <Typography sx={{ fontSize: 16 }} gutterBottom>
-                      {data.name}, Age: {data.age}
-                    </Typography>
-                    <br></br>
-                    <Typography sx={{ fontSize: 14 }}>
-                      Classes Taken:
-                      {data.classesTaken.map((c) => (
-                        <div key={c.id}>{GetClass(classList, c.class.id)}</div>
-                      ))}
-                    </Typography>
-                    <br></br>
-                  </CardContent>
-                </React.Fragment>
-              </Card>
-              <br></br>
-            </div>
-          ))}
-        </Box>
-
-        
-
-
-
+          </div>
+        ))}
+      </Box>
     </div>
   );
 }
