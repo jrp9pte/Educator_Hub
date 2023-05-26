6e4898b0db69c911518069efcf560b0cf36ffba2
Jacob Wald <vug5ue@virginia.edu>
2023-05-26 09:14:09 -0400

    Dashboard now updates students classesTaken array to remove the deleted class from it. Unused imports also removed

--- tje-dashboard-app/src/pages/TeacherDirectory.js
+++ tje-dashboard-app/src/pages/TeacherDirectory.js
@@ -1,48 +1,39 @@
 import TEACHER_DIRECTORY from "../components/Teacher_Directory/Teacher_Directory";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import { onAuthStateChanged } from "firebase/auth";
-import { auth } from '../firebase';
-import {Container, Image} from "react-bootstrap"
+import { auth } from "../firebase";
 import Login from "../components/AUTH/Login";
 
 const TeacherDirectory = () => {
   const [newTeacherName, setNewTeacherName] = useState("");
- 
-  useEffect(()=>{
-      onAuthStateChanged(auth, (user) => {
-          if (user) {
-            // User is signed in, see docs for a list of available properties
-            // https://firebase.google.com/docs/reference/js/firebase.User
-            const uid = user.uid;
-            setNewTeacherName("a");
-            // ...
-            console.log("uid", uid)
-          } else {
-            setNewTeacherName("");
-            // User is signed out
-            // ...
-            console.log("user is logged out")
-          }
-        });
-       
-  }, [])
-  if(newTeacherName === "a") {
-  return (
-    <>
-      <TEACHER_DIRECTORY />
-    </>
-  );
-}
-else {
-  return(
-  <div style={{ textAlign: "center" }}>
- {<Login />} 
-  </div>
-  )
-}
-}
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/firebase.User
+        const uid = user.uid;
+        setNewTeacherName("a");
+        // ...
+        console.log("uid", uid);
+      } else {
+        setNewTeacherName("");
+        // User is signed out
+        // ...
+        console.log("user is logged out");
+      }
+    });
+  }, []);
+  if (newTeacherName === "a") {
+    return (
+      <>
+        <TEACHER_DIRECTORY />
+      </>
+    );
+  } else {
+    return <div style={{ textAlign: "center" }}>{<Login />}</div>;
+  }
+};
 
 export default TeacherDirectory;
